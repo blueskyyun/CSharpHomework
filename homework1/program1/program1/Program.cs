a2e29d8403bae1e20df5c05761f5cc5aa71d2dbc
ZhangRu <z13367163183@outlook.com>
2018-09-12 07:03:55 +0800

    提交第一次作业

---
+++ homework1/program1/program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.WriteLine("请输入第1个数字：");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第2个数字：");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(num1 * num2);
+        }
+    }
+}
