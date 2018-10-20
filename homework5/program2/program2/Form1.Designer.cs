c920e4fed09f32ae2dae894cd6b000dfd03b09bf
ZhangRu <z13367163183@outlook.com>
2018-10-20 20:14:56 +0800

    添加第五次作业

---
+++ homework5/program2/program2/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton12 = new System.Windows.Forms.RadioButton();
+            this.radioButton13 = new System.Windows.Forms.RadioButton();
+            this.radioButton14 = new System.Windows.Forms.RadioButton();
+            this.radioButton15 = new System.Windows.Forms.RadioButton();
+            this.radioButton16 = new System.Windows.Forms.RadioButton();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(3, 656);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.radioButton10);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.radioButton11);
+            this.groupBox2.Controls.Add(this.radioButton12);
+            this.groupBox2.Controls.Add(this.radioButton13);
+            this.groupBox2.Controls.Add(this.radioButton14);
+            this.groupBox2.Controls.Add(this.radioButton15);
+            this.groupBox2.Controls.Add(this.radioButton16);
+            this.groupBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(805, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(284, 687);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "请选择树的颜色";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(224, 54);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "请输入\r\n角度  【10-40(单位:度)】\r\n长度比【0.6-0.8】";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(9, 372);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 28);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "左分支长度比";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(155, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "右分支长度比";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(158, 372);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 28);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(158, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "右分支的角度";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "左分支的角度";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(8, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton10.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(159, 115);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(54, 28);
+            this.radioButton10.TabIndex = 8;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "随机";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton11.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(6, 115);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(117, 28);
+            this.radioButton11.TabIndex = 7;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "紫红色+棕色";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            this.radioButton11.CheckedChanged += new System.EventHandler(this.radioButton11_CheckedChanged);
+            // 
+            // radioButton12
+            // 
+            this.radioButton12.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton12.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton12.AutoSize = true;
+            this.radioButton12.Location = new System.Drawing.Point(55, 290);
+            this.radioButton12.Name = "radioButton12";
+            this.radioButton12.Size = new System.Drawing.Size(6, 6);
+            this.radioButton12.TabIndex = 6;
+            this.radioButton12.TabStop = true;
+            this.radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            this.radioButton13.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton13.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton13.AutoSize = true;
+            this.radioButton13.Location = new System.Drawing.Point(159, 27);
+            this.radioButton13.Name = "radioButton13";
+            this.radioButton13.Size = new System.Drawing.Size(117, 28);
+            this.radioButton13.TabIndex = 5;
+            this.radioButton13.TabStop = true;
+            this.radioButton13.Text = "绿色+灰白色";
+            this.radioButton13.UseVisualStyleBackColor = true;
+            this.radioButton13.CheckedChanged += new System.EventHandler(this.radioButton13_CheckedChanged);
+            // 
+            // radioButton14
+            // 
+            this.radioButton14.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton14.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton14.AutoSize = true;
+            this.radioButton14.Location = new System.Drawing.Point(159, 70);
+            this.radioButton14.Name = "radioButton14";
+            this.radioButton14.Size = new System.Drawing.Size(117, 28);
+            this.radioButton14.TabIndex = 4;
+            this.radioButton14.TabStop = true;
+            this.radioButton14.Text = "枫叶红+棕色";
+            this.radioButton14.UseVisualStyleBackColor = true;
+            this.radioButton14.CheckedChanged += new System.EventHandler(this.radioButton14_CheckedChanged);
+            // 
+            // radioButton15
+            // 
+            this.radioButton15.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton15.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton15.AutoSize = true;
+            this.radioButton15.Location = new System.Drawing.Point(6, 70);
+            this.radioButton15.Name = "radioButton15";
+            this.radioButton15.Size = new System.Drawing.Size(117, 28);
+            this.radioButton15.TabIndex = 3;
+            this.radioButton15.TabStop = true;
+            this.radioButton15.Text = "金黄色+棕色";
+            this.radioButton15.UseVisualStyleBackColor = true;
+            this.radioButton15.CheckedChanged += new System.EventHandler(this.radioButton15_CheckedChanged);
+            // 
+            // radioButton16
+            // 
+            this.radioButton16.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton16.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton16.AutoSize = true;
+            this.radioButton16.Location = new System.Drawing.Point(8, 27);
+            this.radioButton16.Name = "radioButton16";
+            this.radioButton16.Size = new System.Drawing.Size(99, 28);
+            this.radioButton16.TabIndex = 2;
+            this.radioButton16.TabStop = true;
+            this.radioButton16.Text = "绿色+棕色";
+            this.radioButton16.UseVisualStyleBackColor = true;
+            this.radioButton16.CheckedChanged += new System.EventHandler(this.radioButton16_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 687);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton12;
+        private System.Windows.Forms.RadioButton radioButton13;
+        private System.Windows.Forms.RadioButton radioButton14;
+        private System.Windows.Forms.RadioButton radioButton15;
+        private System.Windows.Forms.RadioButton radioButton16;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ homework5/program2/program2/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const double DOUBLE_DELTA = 1E-06;
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            if (!int.TryParse(textBox1.Text, out Th2) || Th2 < 10 || Th2 > 40)//左分支角度
+            {
+                Th2 = 20;
+            }
+            if (!int.TryParse(textBox2.Text, out Th1) || Th1 < 10 || Th1 > 40)//右分支角度
+            {
+                Th1 = 30;
+            }
+            if (!(double.TryParse(textBox3.Text, out per2) && per2-0.8 < DOUBLE_DELTA && per2 - 0.6 > DOUBLE_DELTA))//左分支长度比
+            {
+                per2 = 0.7;
+            }
+            if (!(double.TryParse(textBox4.Text, out per1) && per1 - 0.8 < DOUBLE_DELTA && per1 - 0.6 > DOUBLE_DELTA))//右分支长度比
+            {
+                per1 = 0.6;
+            }
+            drawCayleyTree(10, 500, 510, 100, -Math.PI / 2,width);
+        }
+        private Graphics graphics;
+        static int Th1 = 30;
+        static int Th2 = 20;
+        double th1 = Th1 * Math.PI / 180;
+        double th2 = Th2 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        double k = 0.8;
+        float width = 3f;  //树的粗细
+        Color[] newColor = new Color[5] {
+            Color.Sienna,
+            Color.LimeGreen,
+            Color.LawnGreen,
+            Color.GreenYellow,
+            Color.PaleGreen };
+        
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th,float wth)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double x2 = x0 + leng * k * Math.Cos(th);
+            double y2 = y0 + leng * k * Math.Sin(th);
+            
+            width = (float)(3 - (10 - n) * 0.25);
+
+            if (n <= 10 && n >= 9)
+            {
+
+                drawLine(newColor[0], x0, y0, x1, y1);
+            }
+            else if (n == 8) {
+                drawLine(newColor[1], x0, y0, x1, y1);
+            } else if (n == 7) {
+                drawLine(newColor[2], x0, y0, x1, y1);
+            }
+            else
+            {
+                drawLine(newColor[4 - n%2], x0, y0, x1, y1);
+            }
+
+            drawCayleyTree(n - 1, x1, y1, leng * per1, th + th1,width);
+            drawCayleyTree(n - 1, x2, y2, leng * per2, th - th2, width);
+        }
+
+        void drawLine(Color color,double x0, double y0, double x1, double y1)
+        {
+           Pen pen = new Pen(color, width);
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+      
+        
+        /// <summary>
+        /// 绿色+棕色
+        /// </summary>
+        
+        private void radioButton16_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton16.Checked)
+            {
+                newColor[0] = Color.Sienna;
+                newColor[1] = Color.LimeGreen;
+                newColor[2] = Color.LawnGreen;
+                newColor[3] = Color.GreenYellow;
+                newColor[4] = Color.PaleGreen;
+
+            }
+        }
+        /// <summary>
+        /// 绿色+灰白色
+        /// </summary>
+        private void radioButton13_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton13.Checked)
+            {
+                newColor[0] = Color.Silver;
+                newColor[1] = Color.YellowGreen;
+                newColor[2] = Color.LightGreen;
+                newColor[3] = Color.PaleGreen;
+                newColor[4] = Color.PaleGoldenrod;
+            }
+        }
+        /// <summary>
+        /// 金黄色+棕色
+        /// </summary>
+        private void radioButton15_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton15.Checked)
+            {
+                newColor[0] = Color.SaddleBrown;
+                newColor[1] = Color.Peru;
+                newColor[2] = Color.Tan;
+                newColor[3] = Color.Gold;
+                newColor[4] = Color.Yellow;
+            }
+        }
+        /// <summary>
+        /// 枫叶红+棕色
+        /// </summary>
+        private void radioButton14_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton14.Checked)
+            {
+                newColor[0] = Color.SaddleBrown;
+                newColor[1] = Color.Maroon;
+                newColor[2] = Color.Crimson;
+                newColor[3] = Color.OrangeRed;
+                newColor[4] = Color.Red;
+            }
+        }
+        /// <summary>
+        /// 紫红色+棕色
+        /// </summary>
+        private void radioButton11_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton11.Checked)
+            {
+                newColor[0] = Color.SaddleBrown;
+                newColor[1] = Color.Maroon;
+                newColor[2] = Color.MediumVioletRed;
+                newColor[3] = Color.Firebrick;
+                newColor[4] = Color.DeepPink;
+            }
+        }
+        /// <summary>
+        ///  随机
+        /// </summary>
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton10.Checked)
+            {
+                newColor[0] = Color.FromArgb(new Random(Guid.NewGuid().GetHashCode()).Next(10, 100), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250));
+                newColor[1] = Color.FromArgb(new Random(Guid.NewGuid().GetHashCode()).Next(10, 100), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250));
+                newColor[2] = Color.FromArgb(new Random(Guid.NewGuid().GetHashCode()).Next(10, 100), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250));
+                newColor[3] = Color.FromArgb(new Random(Guid.NewGuid().GetHashCode()).Next(10, 100), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250));
+                newColor[4] = Color.FromArgb(new Random(Guid.NewGuid().GetHashCode()).Next(10, 100), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250), new Random(Guid.NewGuid().GetHashCode()).Next(0, 250));
+
+            }
+        }
+        /// <summary>
+        /// 左分支角度
+        /// </summary>
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
